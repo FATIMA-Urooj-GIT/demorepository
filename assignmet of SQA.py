95cb950c74369a7345a646fb87e695a86dc2cbda
Muqaddas 452 <muqaddasrasheed452@gmail.com>
2026-03-31 18:42:42 +0500

    Added substraction feature

--- assignmet of SQA.py
+++ assignmet of SQA.py
@@ -31,3 +31,4 @@ if __name__ == "__main__":
     num1 = 10
     num2 = 5
     print(f"Addition of {num1} and {num2} is: {num1 + num2}")
+    print("Substraction of {num1} and {num2} is : {num1 - num2}")
